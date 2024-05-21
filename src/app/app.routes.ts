b05a5617aa11da45d176502adf56663fe6213f7e
marciodavid <marciodavid81@gmail.com>
2024-05-20 23:22:00 -0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { CardapioComponent } from './components/cardapio/cardapio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'sobre',
+        component: SobreComponent
+    },
+    {
+        path: 'cardapio',
+        component: CardapioComponent
+    }
+];
